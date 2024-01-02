0269642a0ecec609fe9e50966bc97680374127e1
hnaa0 <hnaa12@daum.net>
2024-01-02 18:52:29 +0900

    Make Slider with Button

--- src/components/ButtonSlider.jsx
+++ src/components/ButtonSlider.jsx
@@ -1,7 +1,16 @@
 import styled from "styled-components";
 import slideImages from "../slideImages";
+import { useState } from "react";
 
 export default function ButtonSlider() {
+  const [slideMove, setSlideMove] = useState(0);
+  const toNext = () => {
+    slideMove > -2128 && setSlideMove((prev) => prev - 1064);
+  };
+  const toPrev = () => {
+    slideMove < 0 && setSlideMove((prev) => prev + 1064);
+  };
+
   return (
     <Container>
       <SlideTitle>Slide with Button</SlideTitle>
@@ -10,6 +19,7 @@ export default function ButtonSlider() {
           {slideImages.map((slideImage) => {
             return (
               <SlideImg
+                $slidemove={slideMove}
                 key={slideImage.title}
                 src={slideImage.img}
                 alt={slideImage.title}
@@ -17,46 +27,51 @@ export default function ButtonSlider() {
             );
           })}
         </SlideBox>
-        <PrevBtn>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-arrow-left"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fillRule="evenodd"
-              d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
-            />
-          </svg>
-        </PrevBtn>
-        <NextBtn>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-arrow-right"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fillRule="evenodd"
-              d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-            />
-          </svg>
-        </NextBtn>
+        {slideMove !== 0 && (
+          <PrevBtn onClick={toPrev}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-arrow-left"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+              />
+            </svg>
+          </PrevBtn>
+        )}
+        {slideMove !== -1064 && (
+          <NextBtn onClick={toNext}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-arrow-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+              />
+            </svg>
+          </NextBtn>
+        )}
       </SlideContainer>
     </Container>
   );
 }
 
 const Container = styled.section`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
-  position: relative;
+  margin-top: 100px;
 `;
 
 const SlideTitle = styled.h1`
@@ -73,6 +88,7 @@ const SlideContainer = styled.div`
 const SlideBox = styled.div`
   display: flex;
   height: 100%;
+  width: 100%;
   padding: 4px 0;
 `;
 
@@ -80,13 +96,14 @@ const SlideImg = styled.img`
   width: 200px;
   height: auto;
   object-fit: cover;
-  margin-right: 16px;
+  margin: 0 8px;
   border-radius: 8px;
   transition: 0.2s;
+  transform: translateX(${(props) => `${props.$slidemove}px`});
 
   &:hover {
     opacity: 0.9;
-    transform: scale(1.025);
+    transform: translateX(${(props) => `${props.$slidemove}px`}) scale(1.025);
     cursor: pointer;
   }
 `;
