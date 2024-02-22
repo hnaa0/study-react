0cbd0869c6770a38ff54e4d7b31e249b1a9782fb
hnaa0 <hnaa12@daum.net>
2024-02-22 12:28:58 +0900

    Edit path

--- src/SlideImages.jsx
+++ src/SlideImages.jsx
@@ -1,35 +1,35 @@
 const slideImages = [
   {
     title: "dog",
-    img: "/images/3dogs.jpg",
+    img: "https://hnaa0.github.io/study-react/images/3dogs.jpg",
   },
   {
     title: "beach",
-    img: "/images/beach.jpg",
+    img: "https://hnaa0.github.io/study-react/images/beach.jpg",
   },
   {
     title: "breads",
-    img: "/images/breads.jpg",
+    img: "https://hnaa0.github.io/study-react/images/breads.jpg",
   },
   {
     title: "fireworks",
-    img: "/images/fireworks.jpg",
+    img: "https://hnaa0.github.io/study-react/images/fireworks.jpg",
   },
   {
     title: "fruits",
-    img: "/images/fruits.jpg",
+    img: "https://hnaa0.github.io/study-react/images/fruits.jpg",
   },
   {
     title: "mountain",
-    img: "/images/mountain.jpg",
+    img: "https://hnaa0.github.io/study-react/images/mountain.jpg",
   },
   {
     title: "street",
-    img: "/images/street.jpg",
+    img: "https://hnaa0.github.io/study-react/images/street.jpg",
   },
   {
     title: "airplane",
-    img: "/images/airplane.jpg",
+    img: "https://hnaa0.github.io/study-react/images/airplane.jpg",
   },
 ];
 
