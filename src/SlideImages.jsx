329cfcc2ebef2d042ce6653a70c4984754b74b3f
hnaa0 <hnaa12@daum.net>
2024-02-22 12:05:40 +0900

    Edit path

--- src/SlideImages.jsx
+++ src/SlideImages.jsx
@@ -1,35 +1,37 @@
+import DOG from "";
+
 const slideImages = [
   {
     title: "dog",
-    img: "src/images/3dogs.jpg",
+    img: "/src/images/3dogs.jpg",
   },
   {
     title: "beach",
-    img: "src/images/beach.jpg",
+    img: "/src/images/beach.jpg",
   },
   {
     title: "breads",
-    img: "src/images/breads.jpg",
+    img: "/src/images/breads.jpg",
   },
   {
     title: "fireworks",
-    img: "src/images/fireworks.jpg",
+    img: "/src/images/fireworks.jpg",
   },
   {
     title: "fruits",
-    img: "src/images/fruits.jpg",
+    img: "/src/images/fruits.jpg",
   },
   {
     title: "mountain",
-    img: "src/images/mountain.jpg",
+    img: "/src/images/mountain.jpg",
   },
   {
     title: "street",
-    img: "src/images/street.jpg",
+    img: "/src/images/street.jpg",
   },
   {
     title: "airplane",
-    img: "src/images/airplane.jpg",
+    img: "/src/images/airplane.jpg",
   },
 ];
 
